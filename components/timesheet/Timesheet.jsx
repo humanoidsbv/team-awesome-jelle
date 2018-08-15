a49b647ed85df87e9bb5028987aee1beb61abf9e
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-08-15 10:58:57 +0200

    Adds a better map function, and added it to the component

--- components/timesheet/Timesheet.jsx
+++ components/timesheet/Timesheet.jsx
@@ -1,32 +1,27 @@
 import React from 'react';
 import TimesheetEntry from '../timesheet-entry/TimesheetEntry';
 
-import data from '../../pages/mock-timesheet-entries.json';
+import timesheetEntries from '../../pages/mock-timesheet-entries.json';
 
 import './timesheet.scss';
 
-const displayTimesheets = data.map((currentTimesheet, index, array) => (
-  (index !== 0 && currentTimesheet.date === array[index - 1].date)
-    ? (
-      <TimesheetEntry
-        {...currentTimesheet}
-      />
-    )
-    : (
-      <React.Fragment>
-        <p className="timesheet-wrapper__date">
-          {currentTimesheet.date}
-        </p>
-        <TimesheetEntry
-          {...currentTimesheet}
-        />
-      </React.Fragment>
-    )
-));
 
 const Timesheet = () => (
   <div className="timesheet-wrapper">
-    {displayTimesheets}
+    {
+      timesheetEntries.map((currentTimesheet, index, array) => (
+        <React.Fragment key={currentTimesheet.id}>
+          {
+            (!index || (currentTimesheet.date !== array[index - 1].date)) && (
+              <p className="timesheet-wrapper__date">
+                {currentTimesheet.date}
+              </p>
+            )}
+          <TimesheetEntry
+            {...currentTimesheet}
+          />
+        </React.Fragment>
+      ))}
   </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './index.scss';
 import TimesheetInput from '../components/timesheet-input/TimesheetInput';
 import Timesheet from '../components/timesheet/Timesheet';
-// import Timesheet from '../components/timesheet/Timesheet';
 
 export default () => (
   <main className="timesheet-page-wrapper">
