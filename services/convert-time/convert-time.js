497b2cc7e70c687590211ebadd9d0d802da1cfa6
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-08-21 13:17:01 +0200

    make convertdate and converttime more generic

--- services/convert-time/convert-time.js
+++ services/convert-time/convert-time.js
@@ -1,30 +1,14 @@
+export const convertDateToIso = date => (
+  date
+    .split('-')
+    .reverse()
+    .join('-')
+);
 
-const convertDateToUs = (date) => {
-  const splittedDate = date.split('-');
-  return `${splittedDate[2]}-${splittedDate[1]}-${splittedDate[0]}`;
-};
-
-
-const convertTime = (prevState) => {
-  const { date, startTime, endTime } = prevState.timeEntry;
-
-  const convertedDate = convertDateToUs(date);
-
-  const convertedStartTime = new Date(`
-    ${convertedDate} ${startTime.replace('.', ':')}
+export const convertTime = (time, date) => {
+  const convertedTime = new Date(`
+    ${convertDateToIso(date)} ${time.replace('.', ':')}
   `).toISOString();
 
-  const convertedEndTime = new Date(`
-    ${convertedDate} ${endTime.replace('.', ':')}
-  `).toISOString();
-
-
-  return {
-    ...prevState.timeEntry,
-    date: convertedDate,
-    startTime: convertedStartTime,
-    endTime: convertedEndTime
-  };
+  return convertedTime;
 };
-
-export default convertTime;
