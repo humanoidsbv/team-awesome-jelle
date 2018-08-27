a9149fb3cd72a7a807af83cdfc5217b75a4212cf
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-08-27 16:58:57 +0200

    re-arrange call function

--- sagas/time-entries.js
+++ sagas/time-entries.js
@@ -16,18 +16,18 @@ import {
 } from '../services/fetch-timesheet-entries/fetch-timesheet-entries';
 
 function* onDeleteTimesheetEntry(action) {
-  yield call(deleteTimesheetEntry(action.timesheetEntryId));
-  yield put(deleteTimesheetEntrySucces(action.timesheetEntryId));
+  yield call(deleteTimesheetEntry, action.timesheetEntryId);
+  yield put(deleteTimesheetEntrySucces, action.timesheetEntryId);
 }
 
 function* onGetTimesheetEntries() {
-  const timesheetEntries = yield call(fetchTimesheetEntries());
-  yield put(requestTimeEntriesSucces(timesheetEntries));
+  const timesheetEntries = yield call(fetchTimesheetEntries);
+  yield put(requestTimeEntriesSucces, timesheetEntries);
 }
 
 function* onPostTimesheetEntry(action) {
-  const timesheetEntry = yield call(postTimesheetEntry(action.timesheetEntry));
-  yield put(postTimesheetEntrySucces(timesheetEntry));
+  const timesheetEntry = yield call(postTimesheetEntry, action.timesheetEntry);
+  yield put(postTimesheetEntrySucces, timesheetEntry);
 }
 
 export default function* watchTimesheetEntries() {
