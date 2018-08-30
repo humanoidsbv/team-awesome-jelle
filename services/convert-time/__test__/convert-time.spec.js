c92cd16b8a0a8f71418b54c0988f1312d89e9aae
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-08-30 10:34:37 +0200

    add third test for converting to iso string

--- services/convert-time/__test__/convert-time.spec.js
+++ services/convert-time/__test__/convert-time.spec.js
@@ -1,5 +1,9 @@
-import { convertDateToIso } from '../convert-time';
+import { convertDateToIso, convertTimeToIso } from '../convert-time';
 
 test('Split, reverse and join Dutch date notation', () => {
   expect(convertDateToIso('01-01-2000')).toBe('2000-01-01');
+});
+
+test('Combine a date and time string to an ISO string', () => {
+  expect(convertTimeToIso('10:00', '01-01-2000')).toBe('2000-01-01T09:00:00.000Z');
 });
