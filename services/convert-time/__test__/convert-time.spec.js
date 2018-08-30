5db28a62736495b1cbd9b8499d9387a5c669d40c
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-08-30 12:12:20 +0200

    rewrite to be able to check placement of date and month

--- services/convert-time/__test__/convert-time.spec.js
+++ services/convert-time/__test__/convert-time.spec.js
@@ -1,9 +1,9 @@
 import { convertDateToIso, convertTimeToIso } from '../convert-time';
 
 test('Split, reverse and join Dutch date notation', () => {
-  expect(convertDateToIso('01-01-2000')).toBe('2000-01-01');
+  expect(convertDateToIso('20-01-2000')).toBe('2000-01-20');
 });
 
 test('Combine a date and time string to an ISO string', () => {
-  expect(convertTimeToIso('10:00', '01-01-2000')).toBe('2000-01-01T09:00:00.000Z');
+  expect(convertTimeToIso('10:00', '20-01-2000')).toBe('2000-01-20T09:00:00.000Z');
 });
