5f256e2a750677e4964e32ccb155a03ac7ab53af
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-08-30 14:48:21 +0200

    add TimesheetEntry component snapshot test

--- components/timesheet-entry/__tests__/TimesheetEntry-test.jsx
+++ components/timesheet-entry/__tests__/TimesheetEntry-test.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
-import { createSerializer } from 'enzyme-to-json';
+import { shallow } from 'enzyme';
 
 import TimesheetEntry from '../TimesheetEntry';
 
-expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+const timesheetEntry = {
+  employer: 'Port of Rotterdam',
+  endTime: '2018-12-13T18:30:00.000Z',
+  id: 1,
+  startTime: '2018-12-13T18:00:00.000Z'
+};
+
+const handleDeleteTimesheetEntry = (timesheetEntryId) => {
+  this.props.onDeleteTimesheetEntry(timesheetEntryId);
+};
 
 it('Renders Entry Component correctly', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TimesheetEntry />, div);
-});
-
-it('renders a snapshot', () => {
-  const tree = renderer.create(<TimesheetEntry />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const wrapper = shallow(
+    <TimesheetEntry
+      employer={timesheetEntry.employer}
+      endTime={timesheetEntry.endTime}
+      id={timesheetEntry.id}
+      onDelete={handleDeleteTimesheetEntry}
+      startTime={timesheetEntry.startTime}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
 });
