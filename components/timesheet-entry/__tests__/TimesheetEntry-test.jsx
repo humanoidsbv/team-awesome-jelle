4d66cc411e3e540ec58d891cc7059a2a0d73c61f
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-08-30 13:02:01 +0200

    install enzym for shallow/deep snapshot testing

--- components/timesheet-entry/__tests__/TimesheetEntry-test.jsx
+++ components/timesheet-entry/__tests__/TimesheetEntry-test.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import { createSerializer } from 'enzyme-to-json';
+
 import TimesheetEntry from '../TimesheetEntry';
+
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
 
 it('Renders Entry Component correctly', () => {
   const div = document.createElement('div');
