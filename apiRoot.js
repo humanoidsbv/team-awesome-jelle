d16448aeb839570b6df7174341cb0f54defad587
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-09-06 11:30:47 +0200

    define differen environment for production or develo

---
+++ apiRoot.js
@@ -0,0 +1,11 @@
+const production = 'production';
+const development = 'development';
+
+const environments = {
+  [production]: 'http://my-json-server.typicode.com/humanoidsbv/team-awesome-jelle/',
+  [development]: 'http://localhost:3001/api/'
+};
+
+const environment = process.env.NODE_ENV === production ? production : development;
+
+export default environments[environment];
