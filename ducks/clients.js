781d7b9bce16d3b606f2a6c9a3ccdb4b810b33ff
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-09-12 12:43:41 +0200

    create reducer, saga, fetchCalls for Clients

---
+++ ducks/clients.js
@@ -0,0 +1,61 @@
+import { createSelector } from 'reselect';
+
+export const REQUEST_CLIENTS = 'GET_CLIENTS';
+export const REQUEST_CLIENTS_SUCCESS = 'GET_CLIENTS_SUCCESS';
+export const POST_CLIENT = 'POST_CLIENT';
+export const POST_CLIENT_SUCCESS = 'POST_CLIENT_SUCCESS';
+
+export const initialState = {
+  items: [],
+  isLoading: false,
+  isSaving: false
+};
+
+const clientsRoot = state => state.clients;
+
+export const clientsItemSelector = createSelector(
+  clientsRoot,
+  clients => clients.items
+);
+
+
+export function clientReducer(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_CLIENTS:
+      return { ...state };
+    case REQUEST_CLIENTS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        items: action.clients
+      };
+    case POST_CLIENT:
+      return { ...state, isFormSaving: true };
+    case POST_CLIENT_SUCCESS:
+      return {
+        ...state,
+        isFormSaving: false,
+        items: [...state.items, action.newClient]
+      };
+    default:
+      return state;
+  }
+}
+
+export const getClients = () => ({
+  type: REQUEST_CLIENTS
+});
+
+export const getClientsSuccess = clients => ({
+  type: REQUEST_CLIENTS_SUCCESS,
+  clients
+});
+
+export const postClient = () => ({
+  type: POST_CLIENT
+});
+
+export const postClientSuccess = newClient => ({
+  type: POST_CLIENT_SUCCESS,
+  newClient
+});
