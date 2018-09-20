109960fbcdc2f7650d68620d3ba98d13d0c74e3c
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-09-20 16:16:23 +0200

    Add info for all components

--- shared/components/select-box/__stories__/index.jsx
+++ shared/components/select-box/__stories__/index.jsx
@@ -11,7 +11,7 @@ import { changeSortByProperty } from '../../../../ducks/team-members';
 const sortBy = 'firstName';
 
 const divStyle = {
-  width: '120px'
+  width: '130px'
 };
 
 storiesOf('SelectBox', module)
@@ -59,6 +59,32 @@ storiesOf('SelectBox', module)
           { label: 'Last name', value: 'lastName' }
         ]}
         type="form"
+      />
+    </div>
+  )));
+
+storiesOf('SelectBox', module)
+  .add('filter', withInfo(`
+  A generic selectbox that accepts an array of option objects, eacht containing a label, and value.
+  The SelectBox then maps over the entire array adding an html option tag with correct values for each option.
+
+  ~~~js
+  <option value={option.value}>option.label</option>
+  ~~~
+
+  Three variations exists, sort and filter are very similair and appear on top of the Clients and TeamMember pages.
+  The third, 'form', applies styling that corresponds with TeamAwesome's form styling.
+
+  `)(() => (
+    <div style={divStyle}>
+      <SelectBox
+        activeValue={sortBy}
+        name="sort-by"
+        onChange={event => changeSortByProperty(event.target.value)}
+        options={[{ label: 'First Name', value: 'firstName' },
+          { label: 'Last name', value: 'lastName' }
+        ]}
+        type="filter"
       />
     </div>
   )));
