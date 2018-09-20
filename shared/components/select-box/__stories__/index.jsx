6eb68fd10fdce2034d30b1aff9a7fd4084f0e286
Jelle Bouwman <jelle.bouwman@humanoids.nl>
2018-09-20 16:12:53 +0200

    Add info element to story

--- shared/components/select-box/__stories__/index.jsx
+++ shared/components/select-box/__stories__/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
 import SelectBox from '../SelectBox';
 
 import '../select-box.scss';
@@ -10,15 +10,55 @@ import { changeSortByProperty } from '../../../../ducks/team-members';
 
 const sortBy = 'firstName';
 
-storiesOf('Button', module)
-  .add('default', () => (
-    <SelectBox
-      activeValue={sortBy}
-      name="sort-by"
-      onChange={event => changeSortByProperty(event.target.value)}
-      options={[{ label: 'First Name', value: 'firstName' },
-        { label: 'Last name', value: 'lastName' }
-      ]}
-      type="sort"
-    />
-  ));
+const divStyle = {
+  width: '120px'
+};
+
+storiesOf('SelectBox', module)
+  .add('default', withInfo(`
+    A generic selectbox that accepts an array of option objects, eacht containing a label, and value.
+    The SelectBox then maps over the entire array adding an html option tag with correct values for each option.
+
+    ~~~js
+    <option value={option.value}>option.label</option>
+    ~~~
+
+    Three variations exists, sort and filter are very similair and appear on top of the Clients and TeamMember pages.
+    The third, 'form', applies styling that corresponds with TeamAwesome's form styling.
+  `)(() => (
+    <div style={divStyle}>
+      <SelectBox
+        activeValue={sortBy}
+        name="sort-by"
+        onChange={event => changeSortByProperty(event.target.value)}
+        options={[{ label: 'First Name', value: 'firstName' },
+          { label: 'Last name', value: 'lastName' }
+        ]}
+      />
+    </div>
+  )));
+storiesOf('SelectBox', module)
+  .add('form', withInfo(`
+  A generic selectbox that accepts an array of option objects, eacht containing a label, and value.
+  The SelectBox then maps over the entire array adding an html option tag with correct values for each option.
+
+  ~~~js
+  <option value={option.value}>option.label</option>
+  ~~~
+
+  Three variations exists, sort and filter are very similair and appear on top of the Clients and TeamMember pages.
+  The third, 'form', applies styling that corresponds with TeamAwesome's form styling.
+
+  `)(() => (
+    <div style={divStyle}>
+      <SelectBox
+        activeValue={sortBy}
+        name="sort-by"
+        onChange={event => changeSortByProperty(event.target.value)}
+        options={[{ label: 'First Name', value: 'firstName' },
+          { label: 'Last name', value: 'lastName' }
+        ]}
+        type="form"
+      />
+    </div>
+  )));
